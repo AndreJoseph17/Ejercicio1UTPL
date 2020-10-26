6e6b529dd7001373f8119da6af120717e9cb3c6d
Andre Joseph Jaramillo <andre.joseph.jaramillo@gmail.com>
2020-10-25 23:08:36 -0500

    Actualizacion de repositorio
    Carga de proyecto web para la materia Desarrollo basado en plataformas web

---
+++ nav.php
@@ -0,0 +1,13 @@
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Información personal<span class="sr-only">(current)</span></a>
+      </li>  
+      <li class="nav-item active">
+        <a class="nav-link" href="aprobadas.php">Lista de Materias aprobadas<span class="sr-only"></span></a>
+      </li> 
+      <li class="nav-item active">
+        <a class="nav-link" href="pendientes.php">Lista de materias que faltan de aprobar<span class="sr-only"></span></a>
+      </li> 
+    </ul>
+  </div>
